a689a9b5fde7bd8c43a8debdc413dd6630322b50
Alexander Rovang <DXRovang@gmail.com>
2022-01-17 10:24:07 -0500

    adds useState/useEffect to Images

--- src/Images.js
+++ src/Images.js
@@ -1,4 +1,16 @@
+import { useState, useEffect } from "react";
+
 const Images = () => {
+
+const [images, setImages] = useState([])
+useEffect(()=>{
+  fetch('https://api.nasa.gov/planetary/apod?api_key=vQhUdNX4zTKwT9WbUi3obFnHgWVxIiqd22jkjiyR')
+  .then(r=>r.json())
+  .then(data=>(
+    setImages(data)
+  ))
+}, [])
+
   return ( 
     <div>
       in Images
